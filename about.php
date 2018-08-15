c9650b9bb42c4f2526b616b29cda12b603a9e7dd
Sergei Kulakov <s.culackow@gmail.com>
2018-08-15 22:22:06 +0300

    Add about.php

--- about.php
+++ about.php
@@ -1,29 +1,29 @@
 <?php
-$name = 'Сергей';
-$age = 54;
-$email = 's.culackow@gmail.com';
-$city = 'Силламяэ';
-$about = 'Учусь в Нетологи на веб-разработчика';
+  $name = 'Сергей';
+  $age = 54;
+  $email = 's.culackow@gmail.com';
+  $city = 'Силламяэ';
+  $about = 'Учусь в Нетологи на веб-разработчика';
 ?>
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <title>Document</title>
 </head>
 <body>
 <h1>Страница пользователя <?= $name ?></h1>
 <dl>
-    <dt>Имя</dt>
-    <dd><?= $name ?></dd>
-    <dt>Возраст</dt>
-    <dd><?= $age ?></dd>
-    <dt>Адрес электронной почты</dt>
-    <dd><a href="mailto:<?= $email ?>"><?= $email ?></a></dd>
-    <dt>Город</dt>
-    <dd><?= $city ?></dd>
-    <dt>О себе</dt>
-    <dd><?= $about ?></dd>
+  <dt>Имя</dt>
+  <dd><?= $name ?></dd>
+  <dt>Возраст</dt>
+  <dd><?= $age ?></dd>
+  <dt>Адрес электронной почты</dt>
+  <dd><a href="mailto:<?= $email ?>"><?= $email ?></a></dd>
+  <dt>Город</dt>
+  <dd><?= $city ?></dd>
+  <dt>О себе</dt>
+  <dd><?= $about ?></dd>
 </dl>
 
 </body>
