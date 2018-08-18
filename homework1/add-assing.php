da512679846dd443a84f4c64d76d2cf95a1c123c
Sergei Kulakov <s.culackow@gmail.com>
2018-08-18 16:46:30 +0300

    Add about.php

---
+++ homework1/add-assing.php
@@ -0,0 +1,52 @@
+<?php
+  // Числовой ряд Фибоначчи
+
+  $num = '';
+  $errorNum = '';
+  if (isset($_GET['num'])) {
+    $x = $_GET['num'];
+  } else {
+    $x = 0;
+  }
+
+  $x1 = 0;
+  $x2 = 1;
+  $i = 1;
+
+  while (true) {
+    if ($x1 > $x) {
+      echo "Задуманное число \"$x\"  НЕ входит в числовой ряд ";
+      break;
+    }
+
+    if ($x1 == $x) {
+      echo "Задуманное число \"$x\" входит в числовой ряд";
+      break;
+    }
+
+    $x3 = $x1;
+    $x1 = $x1 + $x2;
+    $x2 = $x3;
+    $i++;
+  }
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Fibonacci numbers</title>
+</head>
+<body>
+<form method="GET" action="#">
+  <input name="num" placeholder="number">
+  <button type="submit">Go</button>
+</form>
+</body>
+</html>
+
+
+
+
+
+
