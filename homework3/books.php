08c223b047e244c8f8c09041d4cce3b9b10f47bc
Sergei Kulakov <s.culackow@gmail.com>
2018-08-25 00:23:24 +0300

    Редактирование

--- homework3/books.php
+++ homework3/books.php
@@ -1,22 +1,26 @@
 <?php
-//  ini_set('error_reporting', E_ALL);
-//  ini_set('display_errors', 1);
-//  ini_set('display_startup_errors', 1);
-  $query = urlencode('Гарри+Потер');
-  $data = file_get_contents('https://www.googleapis.com/books/v1/volumes?q='.$query);
-  $result = json_decode($data, true);
-echo '<pre>';
-//var_dump($result["items"]);
-foreach($result["items"] as $item){
-  $arr[] = $item;
-  foreach ($arr as $item){
-    $arr2[] = $item["volumeInfo"];
-  }
-  foreach ($arr2 as $item){
-    if ($item["authors"]){
-      $arr3[] = [$item["title"],$item["authors"]];
-    }
 
+  $query = urlencode('Гарри+Потер');
+  $data = file_get_contents('https://www.googleapis.com/books/v1/volumes?q=' . $query);
+  $result = json_decode($data, true);
+
+  foreach ($result["items"] as $item) {
+    $arr[] = $item;
+    foreach ($arr as $item) {
+      $arr2[] = $item["volumeInfo"];
+    }
+    foreach ($arr2 as $item) {
+      if ($item["authors"]) {
+        $arr3[] = [$item["title"], $item["authors"][0]];
+      }
+
+    }
   }
-}
-var_dump($arr3);
+
+  $fp = fopen(__DIR__ . "/books.csv", 'a+');
+  foreach ($arr3 as $value){
+    fputcsv($fp, $value, ';');
+  }
+  fclose($fp);
+  echo '<pre>';
+  var_dump($arr3);
