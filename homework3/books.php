62c21bfbc5c90856b57c0e10bf32019765225d09
Sergei Kulakov <s.culackow@gmail.com>
2018-08-26 10:17:41 +0300

    Редактирование

--- homework3/books.php
+++ homework3/books.php
@@ -1,30 +1,44 @@
 <?php
+  ini_set('error_reporting', E_ALL);
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
   if ($argv[1]) {
+
+
     $query = urlencode($argv[1]);
     $data = file_get_contents('https://www.googleapis.com/books/v1/volumes?q=' . $query);
     $result = json_decode($data, true);
-    $books = [];
-    $i = 0;
-    foreach ($result["items"] as $item) {
-      $arr[] = $item;
-      foreach ($arr as $value) {
-        $volumeInfo[] = $value["volumeInfo"];
 
-        foreach ($volumeInfo as $info) {
-          if ($info["authors"]) {
-            $id = $i++;
-            $books[] = [$id, $info["title"], $info["authors"][0]];
+    if (json_last_error() === 0) {
+      $books = [];
+      $i = 1;
+      foreach ($result["items"] as $item) {
+        $arr[] = $item;
+        foreach ($arr as $value) {
+          $volumeInfo[] = $value["volumeInfo"];
+
+          foreach ($volumeInfo as $info) {
+            if (isset($info["authors"])) {
+              $id = $i++;
+              $books[] = [ $info["title"], $info["authors"][0]];
+
+            }
+
           }
-
         }
       }
-    }
-    $fp = fopen(__DIR__ . "/books.csv", 'a+');
 
-    foreach ($books as $value) {
-      fputcsv($fp, $value, ';');
+      //var_dump($books);
+      $fp = fopen(__DIR__ . "/books.csv", 'a+');
+
+      foreach ($books as $value) {
+        fputcsv($fp, $value, ';');
+      }
+      fclose($fp);
+    }else{
+      echo 'Невалидный json';
     }
-    fclose($fp);
+
   } else {
     echo "Ошибка! Нет названия книги";
   }
