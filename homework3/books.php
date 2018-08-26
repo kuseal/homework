1c8f345ef4111096006a8fcb221e863e51d5340e
Sergei Kulakov <s.culackow@gmail.com>
2018-08-26 11:42:59 +0300

    Редактирование

--- homework3/books.php
+++ homework3/books.php
@@ -1,7 +1,5 @@
 <?php
-  ini_set('error_reporting', E_ALL);
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
+
   if (isset($argv[1])) {
 
     $query = urlencode($argv[1]);
